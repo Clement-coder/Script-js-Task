aba8f8eb42a3f73de78cc8a324f827be4cf0f7e0
Clement Raymond <chinexzy37@gmail.com>
2025-02-28 22:41:11 +0000

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let name = "Clement"
+let lastName = "Raymond"
+let fullName=(name+" "+lastName)
+let score = 20
+
+
+if (score >= 50 && score <60){
+    console.log(fullName+" scores is  D")
+}if (score >= 60 && score <70){
+    console.log(fullName+" scores is C")
+}if (score >= 70 && score <80){
+    console.log(fullName+" scores is B")
+}if (score >= 80 && score <100){
+    console.log(fullName+" scores is A")
+}if (score >= 0 && score <50){
+    console.log(fullName+" scores is F")
+}else {
+    console.log("invalid score")
+};
